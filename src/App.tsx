96abc25ae0b777aaa0f6ecdaa7c093cbbfb37742
abbrshv <a.bobryshov@proton.me>
2023-07-31 21:53:52 +0200

    Remove counter feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { Counter } from "./features/counter/Counter"
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
